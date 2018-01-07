4c9e2eb84b8a7f262d29d31f4cbd40c4ae5c71fc
Dev <amithapa1994@gmail.com>
2018-01-07 11:35:41 +0530

    Static files

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
 def article_list(request):
-    return render(request, "articles/article_list.html")
+    article_list = Article.objects.all().order_by('date')
+    context = {
+        'article_list' :article_list
+    }
+    return render(request, "articles/article_list.html", context=context)
