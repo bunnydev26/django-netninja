5eb4dd44b0aa16a840957f466d01b81a95f498ec
Dev <amithapa1994@gmail.com>
2018-01-10 00:04:43 +0530

    Static Files & Thumbnail Field

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = "article"
 
@@ -8,5 +7,3 @@ urlpatterns = [
     url(r'^$', views.article_list, name="list"),
     url(r'^(?P<slug>[\w\-]+)/$', views.article_details, name="detail"),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
