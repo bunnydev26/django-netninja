5840f596fb461069fa724e01abb51829b3523576
Dev <amithapa1994@gmail.com>
2018-01-17 22:07:20 +0530

    login_required and logging the user out

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+# The following is a decorator which is used to protect 
+# the view from executing if the user is not authenticated
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def article_list(request):
     article_list = Article.objects.all().order_by('date')
@@ -14,3 +18,7 @@ def article_details(request, slug):
 	article = Article.objects.get(slug=slug)
 	context = {'article': article}
 	return render(request, 'articles/article_detail.html', context=context)
+
+@login_required(login_url='/accounts/login/')
+def article_create(request):
+	return render(request, 'articles/article_create.html')
